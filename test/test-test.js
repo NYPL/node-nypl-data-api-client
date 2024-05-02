8c991b6c6941c27ffed5a71df2d32a18047304bc
Paul Beaudoin <contact@paulbeaudoin.com>
2024-05-02 12:01:19 -0400

    Debug travis test issues

--- test/test-test.js
+++ test/test-test.js
@@ -3,7 +3,7 @@ const sinon = require('sinon')
 describe.only('Travis debug test', function () {
   beforeEach(() => {
     sinon.stub(global, 'fetch').callsFake(() => Promise.resolve())
-    console.log('Stubbed fetch? ', fetch, !!fetch.restore)
+    console.log('Stubbed fetch? ', global.fetch, fetch, !!fetch.restore)
   })
 
   afterEach(() => {
