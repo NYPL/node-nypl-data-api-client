d8662681e5a213c9adc1644dcedd09ba6d342e72
Paul Beaudoin <contact@paulbeaudoin.com>
2024-05-02 11:18:25 -0400

    Debug travis test issues

--- test/helper.js
+++ test/helper.js
@@ -18,7 +18,7 @@ beforeEach(() => {
     callback(error, accessToken, refreshToken, results)
   })
 
-  console.log('Stubbing fetch: ', fetch)
+  console.log('Stubbing fetch: ', global.fetch)
   _fetchSpy = sinon.stub(global, 'fetch').callsFake(fixtureForRequest)
 })
 
