c48fdf25005839df5319da142a308d3e1409d6cf
Paul Beaudoin <contact@paulbeaudoin.com>
2024-05-06 14:45:41 -0400

    Remove redundant Promise.resolve/reject calls

--- test/bib-test.js
+++ test/bib-test.js
@@ -11,8 +11,6 @@ describe('Bib test', function () {
 
   describe('Bibs endpoint', function () {
     it('should return a Bib', async () => {
-      console.log('(inside test) Stubbed fetch? ', fetch, !!fetch.restore)
-
       const resp = await client.get('bibs/sierra-nypl/17746307', { authenticate: true })
       expect(resp.data).to.be.a('object')
       expect(resp.data).to.have.a.property('id', '17746307')
